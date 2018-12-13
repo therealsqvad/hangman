69afa93c37002a1f303ef3b30185d349b3c3c983
therealsqvad <nikita_klepov@mail.ru>
2018-12-13 18:44:17 +0700

    Добавьте файлы проекта.

---
+++ Visilitsa/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Visilitsa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(519, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Visilitsa/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visilitsa
+{
+    public partial class Form1 : Form
+    {
+        string word = "НИКИТА";
+        int life = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Size = new Size(500, 500);
+            string str = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";//строковая перем. с алфавитом
+            int posX = 20, posY = 150;//начальные позиции первой строки букв
+            for (int i = 0; i < str.Count(); i++)//создание цикла
+            {
+                Button button = new Button();//метод создания новой кнопки
+                button.BackColor = Color.Aqua;//цвет кнопки
+                button.Text = str[i] + "";//присваивает значение след.буквы
+                button.Click += new EventHandler(this.button_Click);
+                button.Location = new System.Drawing.Point(posX, posY);//рисование кнопки
+                button.Size = new System.Drawing.Size(30, 25);//задание размера кнопки
+                this.Controls.Add(button);//добавление кнопки
+                posX += button.Width;//ширина кнопки
+                if ((i + 1) % 8 == 0)
+                {
+                    posX = 20;
+                    posY += button.Height;//высота кнопки
+                }
+            }
+            label1.Text = "* * * * * *";
+            label2.Text = word;
+
+
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button letter_btn = sender as Button;
+            string guessedLetter = letter_btn.Text;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if(word[i] == guessedLetter[0])
+                {
+
+                    if (i == 0)
+                    {
+                        label2.Text = guessedLetter;
+                        string temp = label1.Text.Remove(0, 1);
+
+                        string ins = temp.Insert(0, guessedLetter);
+                        label1.Text = ins;
+                    }
+                    else
+                    {
+
+                        string temp = label1.Text.Remove(2*i, 1);
+
+                        string ins = temp.Insert(2 * i , guessedLetter);
+                        label1.Text = ins;
+                    }
+                }
+                else
+                {
+                    life--;
+                }
+            }
+            letter_btn.Enabled = false;
+        }
+    }
+}
